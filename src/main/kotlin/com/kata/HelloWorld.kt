10bb472f7046161eef6e8dc7333f527f97b2226b
Pau Manrique <pau.manrique@thoughtworks.com>
2021-12-13 11:29:24 +0100

    add new library

--- src/main/kotlin/com/kata/HelloWorld.kt
+++ src/main/kotlin/com/kata/HelloWorld.kt
@@ -1,5 +1,10 @@
 package com.kata
 
+import kotlinx.datetime.Instant
+
 class HelloWorld {
-    fun greeting() = "Hello World"
+    fun greeting(): String {
+        val instant = Instant.parse("2020-01-01T00:00:00Z")
+        return "Hello World it's $instant"
+    }
 }
--- src/test/kotlin/com/kata/HelloWorldTest.kt
+++ src/test/kotlin/com/kata/HelloWorldTest.kt
@@ -6,6 +6,6 @@ import org.junit.jupiter.api.Assertions.*
 internal class HelloWorldTest {
 
     @Test fun greeting() {
-        assertEquals("Hello World", HelloWorld().greeting())
+        assertEquals("Hello World it's 2020-01-01T00:00:00Z", HelloWorld().greeting())
     }
 }
